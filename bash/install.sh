5ddb05f1f63318f83c0edbc442b65aced0d00638
Fabien Racape <fabien.racape@interdigital.com>
2022-09-26 11:11:53 -0700

    update install bash

--- bash/install.sh
+++ bash/install.sh
@@ -6,10 +6,23 @@ set -eu
 
 TORCH="1.9.1"
 TORCHVISION="0.10.1"
-DETECTRON2="https://dl.fbaipublicfiles.com/detectron2/wheels/cu102/torch1.9/index.html"
+CUDA="cu102"
+DETECTRON2="https://dl.fbaipublicfiles.com/detectron2/wheels/${CUDA}/torch${TORCH::-2}/index.html"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 COMPRESSAI=""
 
+
+#test python version>3.8
+python3 -c "import sys; assert(sys.version_info.major>=3); assert(sys.version_info.minor>=8)"
+if [[ $? -gt 0 ]]
+then
+    echo
+    echo "Your python version needs to be >=3.8"
+    echo
+    exit 2
+fi
+
+#parse args
 while [[ $# -gt 0 ]]
 do
     key="$1"
@@ -18,8 +31,9 @@ do
 cat << _EOF_
 RUN OPTIONS:    [-t|--torch torch version, default="1.9.1"]
                 [-v|--torchvision torchvision version, default="0.10.1"]
-                [-d|--detectron2 location of proper pre-built detectron2 (find at "https://detectron2.readthedocs.io/en/latest/tutorials/install.html#install-pre-built-detectron2-linux-only"), default="https://dl.fbaipublicfiles.com/detectron2/wheels/cu102/torch1.9/index.html"]
-                [-c|--compressai) provide path to compressai source code for import (in editable mode), default: install compressai from PyPI)]
+                [--detectron2_url location of proper pre-built detectron2 (find at "https://detectron2.readthedocs.io/en/latest/tutorials/install.html#install-pre-built-detectron2-linux-only"), if not provided, cuda and="https://dl.fbaipublicfiles.com/detectron2/wheels/cu102/torch1.9/index.html"]
+                [--compressai) provide path to compressai source code for import (in editable mode), default: install compressai from PyPI)]
+                [--cuda) provide cuda version in the form cu11.1 for cuda 11.1, or "cpu", default: "cu102")]
 
 EXAMPLE         [bash install.sh -t "1.9.1" -d https://dl.fbaipublicfiles.com/detectron2/wheels/cpu/torch1.9/index.html]
 _EOF_
@@ -27,17 +41,20 @@ _EOF_
             ;;
         -t|--torch) shift; TORCH="$1"; shift; ;;
         -v|--torchvision) shift; TORCHVISION="$1"; shift; ;;
-        -d|--detectron2) shift; DETECTRON2="$1"; shift; ;;
+        --detectron2_url) shift; DETECTRON2="$1"; shift; ;;
         -c|--compressai) shift; COMPRESSAI="$1"; shift; ;;
+        --cuda) shift; CUDA="$1"; shift; ;;
         *) echo "[ERROR] Unknown parameter $1"; exit; ;;
     esac;
 done;
 
+
 pip install -U pip
-pip install fiftyone jupyter
+pip install fiftyone==0.16.6 jupyter ipython
 pip install torch==${TORCH} torchvision==${TORCHVISION} pytorch-msssim
 pip install detectron2 -f ${DETECTRON2}
 
+pip install pybind11
 if [ "${COMPRESSAI}" = "" ]; then
     pip install compressai
 else
