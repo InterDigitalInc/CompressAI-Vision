a5e7eeac21c7986a4edfd2a99e82c702bc0f85bb
Hyomin Choi <hyomin.choi@interdigital.com>
2022-10-14 11:27:12 -0700

    Clean up the reference model.py for custom model design. Readme.md is updated as well to inform that any classes from CompressAI library can be imported to be resued

--- examples/models/bmshj2018-factorized/model.py
+++ examples/models/bmshj2018-factorized/model.py
@@ -27,182 +27,20 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import os
 import warnings
 
-import os
-import torch
-import torch.nn as nn
-from torch import Tensor
 from typing import Dict
 
-# from compressai.ans import BufferedRansEncoder, RansDecoder
-from compressai.entropy_models import EntropyBottleneck
+import torch
+import torch.nn as nn
+
 from compressai.layers import GDN
-# from compressai.registry import register_model # TODO: remove
+from compressai.models.google import CompressionModel
+from compressai.models.utils import conv, deconv
+from torch import Tensor
 
 
-def conv(in_channels, out_channels, kernel_size=5, stride=2):
-    return nn.Conv2d(
-        in_channels,
-        out_channels,
-        kernel_size=kernel_size,
-        stride=stride,
-        padding=kernel_size // 2,
-    )
-
-
-def deconv(in_channels, out_channels, kernel_size=5, stride=2):
-    return nn.ConvTranspose2d(
-        in_channels,
-        out_channels,
-        kernel_size=kernel_size,
-        stride=stride,
-        output_padding=stride - 1,
-        padding=kernel_size // 2,
-    )
-
-def find_named_buffer(module, query):
-    """Helper function to find a named buffer. Returns a `torch.Tensor` or `None`
-
-    Args:
-        module (nn.Module): the root module
-        query (str): the buffer name to find
-
-    Returns:
-        torch.Tensor or None
-    """
-    return next((b for n, b in module.named_buffers() if n == query), None)
-
-def _update_registered_buffer(
-    module,
-    buffer_name,
-    state_dict_key,
-    state_dict,
-    policy="resize_if_empty",
-    dtype=torch.int,
-):
-    new_size = state_dict[state_dict_key].size()
-    registered_buf = find_named_buffer(module, buffer_name)
-
-    if policy in ("resize_if_empty", "resize"):
-        if registered_buf is None:
-            raise RuntimeError(f'buffer "{buffer_name}" was not registered')
-
-        if policy == "resize" or registered_buf.numel() == 0:
-            registered_buf.resize_(new_size)
-
-    elif policy == "register":
-        if registered_buf is not None:
-            raise RuntimeError(f'buffer "{buffer_name}" was already registered')
-
-        module.register_buffer(buffer_name, torch.empty(new_size, dtype=dtype).fill_(0))
-
-    else:
-        raise ValueError(f'Invalid policy "{policy}"')
-
-
-def update_registered_buffers(
-    module,
-    module_name,
-    buffer_names,
-    state_dict,
-    policy="resize_if_empty",
-    dtype=torch.int,
-):
-    """Update the registered buffers in a module according to the tensors sized
-    in a state_dict.
-
-    (There's no way in torch to directly load a buffer with a dynamic size)
-
-    Args:
-        module (nn.Module): the module
-        module_name (str): module name in the state dict
-        buffer_names (list(str)): list of the buffer names to resize in the module
-        state_dict (dict): the state dict
-        policy (str): Update policy, choose from
-            ('resize_if_empty', 'resize', 'register')
-        dtype (dtype): Type of buffer to be registered (when policy is 'register')
-    """
-    valid_buffer_names = [n for n, _ in module.named_buffers()]
-    for buffer_name in buffer_names:
-        if buffer_name not in valid_buffer_names:
-            raise ValueError(f'Invalid buffer name "{buffer_name}"')
-
-    for buffer_name in buffer_names:
-        _update_registered_buffer(
-            module,
-            buffer_name,
-            f"{module_name}.{buffer_name}",
-            state_dict,
-            policy,
-            dtype,
-        )
-
-
-class CompressionModel(nn.Module):
-    """Base class for constructing an auto-encoder with at least one entropy
-    bottleneck module.
-
-    Args:
-        entropy_bottleneck_channels (int): Number of channels of the entropy
-            bottleneck
-    """
-
-    def __init__(self, entropy_bottleneck_channels, init_weights=None):
-        super().__init__()
-        self.entropy_bottleneck = EntropyBottleneck(entropy_bottleneck_channels)
-
-        if init_weights is not None:
-            warnings.warn(
-                "init_weights was removed as it was never functional",
-                DeprecationWarning,
-            )
-
-    def aux_loss(self):
-        """Return the aggregated loss over the auxiliary entropy bottleneck
-        module(s).
-        """
-        aux_loss = sum(
-            m.loss() for m in self.modules() if isinstance(m, EntropyBottleneck)
-        )
-        return aux_loss
-
-    def forward(self, *args):
-        raise NotImplementedError()
-
-    def update(self, force=False):
-        """Updates the entropy bottleneck(s) CDF values.
-
-        Needs to be called once after training to be able to later perform the
-        evaluation with an actual entropy coder.
-
-        Args:
-            force (bool): overwrite previous values (default: False)
-
-        Returns:
-            updated (bool): True if one of the EntropyBottlenecks was updated.
-
-        """
-        updated = False
-        for m in self.children():
-            if not isinstance(m, EntropyBottleneck):
-                continue
-            rv = m.update(force=force)
-            updated |= rv
-        return updated
-
-    def load_state_dict(self, state_dict):
-        # Dynamically update the entropy bottleneck buffers related to the CDFs
-        update_registered_buffers(
-            self.entropy_bottleneck,
-            "entropy_bottleneck",
-            ["_quantized_cdf", "_offset", "_cdf_length"],
-            state_dict,
-        )
-        super().load_state_dict(state_dict)
-
-
-# @register_model("bmshj2018-factorized") # TODO: remove
 class FactorizedPrior(CompressionModel):
     r"""Factorized Prior model from J. Balle, D. Minnen, S. Singh, S.J. Hwang,
     N. Johnston: `"Variational Image Compression with a Scale Hyperprior"
