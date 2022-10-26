2c87308de573ecc2af518e03bb4736e9a0b88d44
Sampsa Riikonen <sampsa.riikonen@iki.fi>
2022-10-26 13:48:09 +0300

    no slices test

---
+++ scripts/tests/14_detectron2_eval_compressai_no_slice.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# tests an evaluation pipeline with a compression model from Compressai and
+# object detection using detectron2
+set -e
+
+echo
+echo 14
+echo
+
+compressai-vision detectron2-eval --y --dataset-name=mpeg-vcm-detection-dummy \
+--scale=100 \
+--progress=1 \
+--compressai-model-name=bmshj2018-factorized \
+--qpars=1 \
+--gt-field=detections \
+--output=detectron2_seg_test.json \
+--model=COCO-InstanceSegmentation/mask_rcnn_X_101_32x8d_FPN_3x.yaml
