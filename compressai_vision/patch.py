f9c1a91e1bb6f512fe8a5734d31c98dbd5ba6a92
Sampsa Riikonen <sampsa.riikonen@iki.fi>
2022-11-18 19:24:27 +0200

    tvd image datasets, finally

--- compressai_vision/patch.py
+++ compressai_vision/patch.py
@@ -31,15 +31,23 @@
 """
 # import importhook # this module simply ...ks up everything (at least torch imports)
 from datetime import datetime
+from importlib.metadata import version
+import fiftyone as fo # importing fiftyone for the first time always takes time as it starts the mongodb
 
-import fiftyone  # importing fiftyone for the first time always takes time as it starts the mongodb
 
+if version("fiftyone") != "0.16.6":
+    print("")
+    print("*** WARNING *** WARNING *** WARNING *** WARNING ***")
+    print("PATCHING FOR FIFTYONE VERSION 0.16.6 BUT YOUR VERSION IS", version("fiftyone"))
+    print("")
 
+"""as per
+https://github.com/voxel51/fiftyone/blob/develop/fiftyone/core/dataset.py#L5616
+https://github.com/voxel51/fiftyone/issues/2096
+
+relevant when sending parallel queue jobs
+"""
 def _make_sample_collection_name(patches=False, frames=False, clips=False):
-    # as per
-    # https://github.com/voxel51/fiftyone/blob/develop/fiftyone/core/dataset.py#L5616
-    # https://github.com/voxel51/fiftyone/issues/2096
-    #
     # conn = foo.get_db_conn()
     now = datetime.now()
 
@@ -60,5 +68,34 @@ def _make_sample_collection_name(patches=False, frames=False, clips=False):
     return name
 
 
+"""as per 
+https://github.com/voxel51/fiftyone/issues/2291
+
+relevant when importing non-canonical OpenImageV6 formats into fiftyone
+"""
+import csv
+import pandas as pd
+def _parse_csv(filename, dataframe=False, index_col=None):
+    if dataframe:
+        data = pd.read_csv(
+            filename, 
+            index_col=index_col,
+            dtype={'ImageID': str}
+            )
+    else:
+        with open(filename, "r", newline="", encoding="utf8") as csvfile:
+            dialect = csv.Sniffer().sniff(csvfile.read(10240))
+            csvfile.seek(0)
+            if dialect.delimiter in fo.utils.openimages._CSV_DELIMITERS:
+                reader = csv.reader(csvfile, dialect)
+            else:
+                reader = csv.reader(csvfile)
+
+            data = [row for row in reader]
+
+    return data
+
 # apply patches
-fiftyone.core.dataset._make_sample_collection_name = _make_sample_collection_name
+fo.core.dataset._make_sample_collection_name = _make_sample_collection_name
+import fiftyone.utils.openimages as fouo
+fouo._parse_csv = _parse_csv
