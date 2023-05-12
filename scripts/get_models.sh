8d2e02cf79830f5d24b9a8cb00444b7bb9a0afa1
Fabien Racape <fabien.racape@interdigital.com>
2023-05-12 16:36:43 -0700

    add license and script to install models, detectron2 for now: wip

---
+++ scripts/get_models.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# This clones and build model architectures and gets pretrained weights
+set -eu
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+mkdir -p "${SCRIPT_DIR}/../models"
+
+## Detectron2
+
+# clone
+if [ -z "$(ls -A models/detectron2)" ]; then
+    git clone https://github.com/facebookresearch/detectron2.git models/detectron2
+fi
+cd models/detectron2
+
+# to be compatible with MPEG FCVCM
+git checkout 175b2453c2bc4227b8039118c01494ee75b08136
+python3 -m pip install -U torch
+python3 -m pip install .
+
+#downaload weights
+mkdir -p "detectron2/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x/139173657"
+wget https://dl.fbaipublicfiles.com/detectron2/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x/139173657/model_final_68b088.pkl \
+ weights/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x/139173657
+
+cd ../../
+
+## JDE
+# mkdir -p "${SCRIPT_DIR}/../models/jde"
