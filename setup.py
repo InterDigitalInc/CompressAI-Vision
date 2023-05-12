a06356ae93983e84ae18444d254ebf558ec3dd83
Fabien Racape <fabien.racape@interdigital.com>
2023-05-12 16:35:26 -0700

    add setup and toml files

---
+++ setup.py
@@ -0,0 +1,92 @@
+# Copyright (c) 2021-2022, InterDigital Communications, Inc
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted (subject to the limitations in the disclaimer
+# below) provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of InterDigital Communications, Inc nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY
+# THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+# CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT
+# NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+# only way to import "fastentrypoint.py" from this directory:
+
+# The following line is modified by setver.bash
+version = "0.1.0.dev0"
+
+this_folder = Path(__file__).resolve().parent
+
+path = this_folder / "requirements.txt"
+install_requires = []
+if path.is_file():
+    with path.open("r") as f:
+        install_requires = f.read().splitlines()
+
+TEST_REQUIRES = ["pytest", "pytest-cov"]
+DEV_REQUIRES = TEST_REQUIRES + [
+    "black",
+    "flake8",
+    "flake8-bugbear",
+    "flake8-comprehensions",
+    "isort",
+    "mypy",
+]
+
+
+def get_extra_requirements():
+    extras_require = {
+        "dev": DEV_REQUIRES,
+        "doc": ["sphinx", "furo", "sphinxcontrib-mermaid==0.7.1"],
+    }
+    extras_require["all"] = {req for reqs in extras_require.values() for req in reqs}
+    return extras_require
+
+
+setup(
+    name="compressai-vision",
+    version=version,
+    install_requires=install_requires,
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "compressai-vision = compressai_vision.cli.main:main",
+        ]
+    },
+    # metadata for upload to PyPI
+    author="CompressAI-Vision team",
+    author_email="compressai@interdigital.com",
+    description="Evaluation pipelines for Video Compression for Machine Vision on top of CompressAI",
+    extras_require=get_extra_requirements(),
+    license="BSD 3-Clause Clear License",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
