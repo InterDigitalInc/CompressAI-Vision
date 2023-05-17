c0b648955cf0d5ddcd42333cc22990ab84654516
Fabien Racape <fabien.racape@interdigital.com>
2023-05-17 15:15:52 -0700

    add possibility to check cuda for torch tools

--- scripts/get_models.sh
+++ scripts/get_models.sh
@@ -21,7 +21,18 @@ git checkout 175b2453c2bc4227b8039118c01494ee75b08136
 # pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu118
 # is there a way this script capture the current nvcc version and depending on
 # the version, propose a specific version torch installation?
-python3 -m pip install -U torch
+
+TORCH_VERSION="2.0.0"
+CUDA_VERSION=$(nvcc --version | sed -n 's/^.*release \([0-9]\+\.[0-9]\+\).*$/\1/p')
+if [ -z "$CUDA_VERSION" ]; then
+    echo "no CUDA detected, installing cpu"
+    pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+else
+    echo "cuda version: $CUDA_VERSION"
+    python3 -m pip install -U torch==${TORCH_VERSION}+cu${CUDA_VERSION//./}$ torchvision==0.15.1+cu${CUDA_VERSION//./} torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu${CUDA_VERSION//./}
+fi
+
+exit 1
 python3 -m pip install .
 
 #downaload weights
