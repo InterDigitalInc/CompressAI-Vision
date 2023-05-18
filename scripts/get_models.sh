1d9d7068b0edd259c091f95986291acd49fe8ba5
Fabien Racape <fabien.racape@interdigital.com>
2023-05-17 22:54:34 -0700

    fix get models script

--- scripts/get_models.sh
+++ scripts/get_models.sh
@@ -11,9 +11,9 @@ mkdir -p "${SCRIPT_DIR}/../models"
 
 # clone
 if [ -z "$(ls -A models/detectron2)" ]; then
-    git clone https://github.com/facebookresearch/detectron2.git models/detectron2
+    git clone https://github.com/facebookresearch/detectron2.git ${SCRIPT_DIR}/models/detectron2
 fi
-cd models/detectron2
+cd ${SCRIPT_DIR}/models/detectron2
 
 # to be compatible with MPEG FCVCM
 git checkout 175b2453c2bc4227b8039118c01494ee75b08136
@@ -25,22 +25,24 @@ git checkout 175b2453c2bc4227b8039118c01494ee75b08136
 TORCH_VERSION="2.0.0"
 CUDA_VERSION=$(nvcc --version | sed -n 's/^.*release \([0-9]\+\.[0-9]\+\).*$/\1/p')
 if [ -z "$CUDA_VERSION" ]; then
-    echo "no CUDA detected, installing cpu"
+    echo "no CUDA detected, installing on cpu"
     pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
 else
     echo "cuda version: $CUDA_VERSION"
     python3 -m pip install -U torch==${TORCH_VERSION}+cu${CUDA_VERSION//./}$ torchvision==0.15.1+cu${CUDA_VERSION//./} torchaudio==2.0.1 --index-url https://download.pytorch.org/whl/cu${CUDA_VERSION//./}
 fi
 
-exit 1
 python3 -m pip install .
 
-#downaload weights
-mkdir -p "detectron2/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x/139173657"
-wget https://dl.fbaipublicfiles.com/detectron2/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x/139173657/model_final_68b088.pkl \
- weights/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x/139173657
-
 cd ../../
+
+#downaload weights
+WEIGHT_DIR="weights/detectron2/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x/139173657"
+mkdir -p ${WEIGHT_DIR}
+wget https://dl.fbaipublicfiles.com/detectron2/COCO-Detection/faster_rcnn_X_101_32x8d_FPN_3x/139173657/model_final_68b088.pkl \
+ ${WEIGHT_DIR}
+
+
 
 ## JDE
 # mkdir -p "${SCRIPT_DIR}/../models/jde"
