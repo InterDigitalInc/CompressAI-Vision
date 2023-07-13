- simplify codecs and corresponding registry - enable user to provide path to dataset in default perf script - minor cleanups - fix inverse normalization

compressai_vision/codec/anchors.py
@@ -28,29 +28,21 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import errno
-import glob
 import logging
 import os
-import shlex
-import shutil
 import subprocess
 import sys
 import time
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Union
 
+import torch.nn as nn
+
 from compressai_vision.model_wrappers import BaseWrapper
 from compressai_vision.registry import register_codec
 from compressai_vision.utils.dataio import PixelFormat, readwriteYUV
 
-from .base import EncoderDecoder
-from .utils import (
-    MIN_MAX_DATASET,
-    feature_pyramid_to_frame,
-    frame_to_feature_pyramid,
-    min_max_inv_normalization,
-    min_max_normalization,
-)
+from .utils import MIN_MAX_DATASET, min_max_inv_normalization, min_max_normalization
 
 
 def get_filesize(filepath: Union[Path, str]) -> int:
@@ -77,8 +69,8 @@ def run_cmdline(cmdline: List[Any], logpath: Optional[Path] = None) -> None:
 
 
 @register_codec("vtm")
-class VTMEncoderDecoder(EncoderDecoder):
-    """EncoderDecoder class for VVC - VTM reference software"""
+class VTM(nn.Module):
+    """Encoder/Decoder class for VVC - VTM reference software"""
 
     def __init__(
         self,
@@ -100,8 +92,10 @@ class VTMEncoderDecoder(EncoderDecoder):
         self.qp = kwargs["encoder_config"]["qp"]
 
         self.dump_yuv = kwargs["dump_yuv"]
-        # TODO (fracape) might not need class var
+        # TODO (fracape) hacky, create separate function with LUT
         self.dataset = dataset_name
+        if "sfu" in dataset_name:
+            self.dataset = "SFU"
         self.vision_model = vision_model
         self.bitstream_dir = Path(kwargs["bitstream_dir"])
         if not self.bitstream_dir.is_dir():
@@ -220,6 +214,9 @@ class VTMEncoderDecoder(EncoderDecoder):
         # TODO (racapef) video: manage frame indexes
         rec_yuv = self.yuvio.read_single_frame(0)
 
+        minv, maxv = self.min_max_dataset
+        rec_yuv = min_max_inv_normalization(rec_yuv, minv, maxv, bitdepth=10)
+
         # TODO (fracape) should feature sizes be part of bitstream?
         features = self.vision_model.reshape_frame_to_feature_pyramid(
             rec_yuv,
@@ -227,6 +224,7 @@ class VTMEncoderDecoder(EncoderDecoder):
             self.subframe_heights,
             packing_all_in_one=True,
         )
+
         # features = {
         #     "data": self.vision_model.reshape_frame_to_feature_pyramid(
         #         rec_yuv,
@@ -241,8 +239,8 @@ class VTMEncoderDecoder(EncoderDecoder):
 
 
 @register_codec("hm")
-class HMEncoderDecoder(VTMEncoderDecoder):
-    """EncoderDecoder class for HEVC - HM reference software"""
+class HM(VTM):
+    """Encoder / Decoder class for HEVC - HM reference software"""
 
     def __init__(
         self,

compressai_vision/codec/base.py
@@ -30,82 +30,57 @@
 import logging
 import math
 from pathlib import Path
-from typing import Dict, List, Tuple
+from typing import Dict, Tuple
 
-from torch import Tensor
+import torch.nn as nn
+
+from compressai_vision.registry import register_codec
 
 
-class EncoderDecoder:
-    """
-    Compresses input tensors of features
-    """
+@register_codec("bypass")
+class Bypass(nn.Module):
+    """Does no encoding/decoding whatsoever. Use for debugging."""
 
-    def __init__(self):
+    def __init__(self, **kwargs):
         self.logger = logging.getLogger(self.__class__.__name__)
-        self.reset()
-        self.compute_metrics = True
+        output_dir = Path(kwargs["output_dir"])
+        if not output_dir.is_dir():
+            self.logger.info(f"creating output folder: {output_dir}")
+            output_dir.mkdir(parents=True, exist_ok=True)
 
-    def computeMetrics(self, state: bool):
-        self.compute_metrics = state
-
-    def getMetrics(self):
-        """returns tuple with (psnr, mssim) from latest encode+decode calculation"""
-        return None, None
-
-    def reset(self):
-        """Reset the internal state of the encoder & decoder, if there is any"""
-        self.cc = 0
-
-    @staticmethod
-    def getOrgInputSize(dSize: Dict) -> Tuple:
-        width = dSize["width"]
-        height = dSize["height"]
-
-        return (width, height)
-
-    @staticmethod
-    def setOrgInputSize(dSize: Tuple) -> Dict:
-        width, height = dSize
-
-        return {"width": width, "height": height}
-
-    @staticmethod
-    def getInputSize(dSize: List) -> Tuple:
-        return dSize[0]
-
-    @staticmethod
-    def setInputSize(dSize: Tuple) -> List:
-        return [
-            dSize,
-        ]
-
-    # TODO (fracape) need for forward function?
-    # ie compress/decompress with entropy estimation a la CompressAI
-
-    # def __call__(self, input: Dict[Tensor]) -> Tuple(List, Dict):
-    #     """
-    #     Encoder/Decoder pipeline
-    #     - Applies pre-processing if need be (e.g. conversion to YUV)
-    #     - Encodes features data
-    #     - Writes a bitstream
-    #     Returns a list of bits per frame and decompressed Dict of Tensors
-    #     """
-
-    def compress(self, input: Dict[str, Tensor]) -> List:
+    def encode(
+        self,
+        input: Dict,
+        file_prefix: str = "",
+    ) -> Dict:
         """
-        Encoder pipeline
-        - Applies pre-processing if need be (e.g. conversion to YUV)
-        - Encodes features data
-        - Writes a bitstream
-        Returns a list of bits per frame
+        Bypass encoder
+        Returns the input and calculates its raw size
         """
-        raise (AssertionError("virtual"))
+        del file_prefix  # used in other codecs that write bitstream files
 
-    def decompress(Self, bitstream_path: str) -> Dict[str, Tensor]:
-        """
-        Decoder pipeline
-        - Decodes features data
-        - Converts back to dictionary of tensors (e.g. conversion to YUV)
-        Returns a list of bits per frame
-        """
-        raise (AssertionError("virtual"))
+        total_elements = 0
+        for ft in input["data"].values():
+            total_elements += _number_of_elements(ft.size())
+
+        # write input
+        total_bytes = total_elements * 4  # 32-bit floating
+
+        return {
+            "bytes": [
+                total_bytes,
+            ],
+            "bitstream": input,
+        }
+
+    def decode(
+        self,
+        input: Dict,
+        file_prefix: str = "",
+    ):
+        del file_prefix  # used in other codecs that write bitstream files
+        return input
+
+
+def _number_of_elements(data: Tuple):
+    return math.prod(data)

compressai_vision/pipelines/split_inference/base_split.py
@@ -41,11 +41,11 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from compressai_vision.codec import EncoderDecoder
-from compressai_vision.evaluators import BaseEvaluator
+# from compressai_vision.evaluators import BaseEvaluator
 from compressai_vision.model_wrappers import BaseWrapper
-from compressai_vision.registry import register_pipeline
-from compressai_vision.utils import dataio
+
+# from compressai_vision.registry import register_pipeline
+# from compressai_vision.utils import dataio
 
 EXT = ".h5"
 

compressai_vision/registry/registers.py
@@ -34,14 +34,12 @@ import torch.nn as nn
 from torch.utils.data import Dataset
 from torchvision import transforms
 
-from compressai_vision.codec import EncoderDecoder
-
 PIPELINES: Dict[str, Callable[..., nn.Module]] = {}
 DATACATALOGS: Dict[str, Callable[..., Any]] = {}
 DATASETS: Dict[str, Callable[..., Dataset]] = {}
 VISIONMODELS: Dict[str, Callable[..., nn.Module]] = {}
 EVALUATORS: Dict[str, Callable[..., nn.Module]] = {}
-CODECS: Dict[str, Callable[..., EncoderDecoder]] = {}
+CODECS: Dict[str, Callable[..., nn.Module]] = {}
 
 TRANSFORMS: Dict[str, Callable[..., Callable]] = {
     k: v for k, v in transforms.__dict__.items() if k[0].isupper()
@@ -52,7 +50,7 @@ TDataset_b = TypeVar("TDataset_b", bound=Dataset)
 TVisionModel_b = TypeVar("TVisionModel_b", bound=nn.Module)
 TEvaluator_b = TypeVar("TEvaluator_b", bound=nn.Module)
 TPipeline_b = TypeVar("TPipeline_b", bound=nn.Module)
-TCodec_b = TypeVar("TCodec_b", bound=EncoderDecoder)
+TCodec_b = TypeVar("TCodec_b", bound=nn.Module)
 
 
 def register_datacatalog(name: str):