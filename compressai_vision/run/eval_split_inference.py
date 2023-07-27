e71444e7a9d3e9af2748d266f19f4e02e103f9e3
Eimran Eimon <eimran.eimon@kopspgd16p.interdigital.com>
2023-07-27 18:57:34 -0400

    cleanup

--- compressai_vision/run/eval_split_inference.py
+++ compressai_vision/run/eval_split_inference.py
@@ -123,11 +123,21 @@ def main(conf: DictConfig):
     coded_res_df = pd.DataFrame(coded_res)
 
     print("=" * 100)
-<<<<<<< HEAD
     print(f"Encoding Information [Top 5 Rows...][{pipeline}]")
-=======
+    print(f"Encoding Information [{pipeline}]")
+    coded_res_df["file_name"] = coded_res_df["file_name"].apply(lambda x: Path(x).name)
+    print(
+        tabulate(coded_res_df, headers="keys", tablefmt="fancy_grid", stralign="center")
+    )
+
+    print("Evaluation Performance")
+    print(tabulate([performance], tablefmt="psql"))
+
+    coded_res, performance = pipeline(**modules)
+    coded_res_df = pd.DataFrame(coded_res)
+
+    print("=" * 100)
     print("Encoding Information")
->>>>>>> f2f391b (fancy output)
     coded_res_df["file_name"] = coded_res_df["file_name"].apply(lambda x: Path(x).name)
     coded_res_df["total_pixels"] = coded_res_df["org_input_size"].apply(
         lambda x: int(x.split("x")[0]) * int(x.split("x")[1])
