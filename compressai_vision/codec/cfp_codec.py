7c1386afd5ad8bb6d43dc3f7a676b5ef0f929f81
Eimran Eimon <eimran.eimon@kopspgpu05p.interdigital.com>
2023-08-03 16:19:45 -0400

    zeroed out redundant fmaps

--- compressai_vision/codec/cfp_codec.py
+++ compressai_vision/codec/cfp_codec.py
@@ -3,17 +3,17 @@ from typing import Dict, Tuple
 
 import numpy as np
 import torch
-import torch.nn as nn
 from scipy.cluster.hierarchy import cut_tree, linkage
 
 from compressai_vision.codec import Bypass
-from compressai_vision.model_wrappers import BaseWrapper
 from compressai_vision.registry import register_codec
 
 
 @register_codec("cfp_codec")
 class CFP_CODEC(Bypass):
-    """Encoder / Decoder class for FCVCM CfP"""
+    """
+    CfP  encoder
+    """
 
     def __init__(
         self,
@@ -26,26 +26,22 @@ class CFP_CODEC(Bypass):
         input: Dict,
         file_prefix: str = "",
     ) -> Dict:
-        """
-        CfP  encoder
-        Returns the input and calculates its raw size
-        """
-        total_elements = 0
+        del file_prefix
+
         # TODO: Dynamicly generate these number using information theory, maybe a RDO? ^_^
-        cluster_number = {
-            "p2": 128,
-            "p3": 128,
-            "p4": 150,
-            "p5": 180,
-        }  # OpenImage Det & Seg
-        # cluster_number = {"p2": 80, "p3": 180, "p4": 190, "p5": 2} # SFU-Traffic
+        # cluster_number = {
+        #     "p2": 128,
+        #     "p3": 128,
+        #     "p4": 150,
+        #     "p5": 180,
+        # }  # OpenImage Det & Seg
+        cluster_number = {"p2": 80, "p3": 180, "p4": 190, "p5": 2}  # SFU-Traffic
+        # cluster_number ={105: 64, 90: 256, 75: 512} # yolo
         # cluster_number = {"p2": 40, "p3": 256, "p4": 256, "p5": 256}
 
-        del file_prefix  # used in other codecs that write bitstream files
-        result_data_dict = input.copy()
+        total_elements = 0
+        result_data_dict = {}
 
-        loss = nn.MSELoss()
-        gram_matrix_loss = []
         for layer_name, layer_data in input["data"].items():
             N, C, H, W = layer_data.size()
             layer_data_np = layer_data.detach().cpu().numpy()
@@ -53,46 +49,22 @@ class CFP_CODEC(Bypass):
             gram_matrix = self._get_gram_matrix(layer_data)
             cluster_labels = self._get_cluster_labels(
                 gram_matrix, cluster_number[layer_name]
-            )  # change cluster no
-            cluster_dict = self._get_cluster_dict(cluster_labels)
-            representive_samples_dict = self._get_representive_sample_from_cluster(
-                cluster_dict, layer_data_np
             )
-
-            for cluster_no, feature_map_no in cluster_dict.items():
-                for i in range(N):
-                    for j in feature_map_no:
-                        layer_data_np[i][j][:][:] = representive_samples_dict[
-                            cluster_no
-                        ]
-
-            N_hat, C_hat, H_hat, W_hat = layer_data_np.shape
-
-            assert N == N_hat
-            assert C == C_hat
-            assert H == H_hat
-            assert W == W_hat
+            cluster_dict = self._get_cluster_dict(cluster_labels)
+            (
+                cluster_dict,
+                representive_samples_dict,
+            ) = self._get_representive_sample_from_cluster(cluster_dict, layer_data_np)
 
             for ft in representive_samples_dict.values():
                 total_elements += _number_of_elements(ft.shape)
 
-            result_data_dict["data"].update(
-                {layer_name: torch.from_numpy(layer_data_np).to("cuda")}
-            )
-            gram_matrix_hat = self._get_gram_matrix(
-                result_data_dict["data"][layer_name]
-            )
-            gram_matrix_loss.append(
-                loss(gram_matrix, gram_matrix_hat).detach().cpu().numpy()
-            )
+            result_data_dict[layer_name] = {}
+            result_data_dict[layer_name]["original_shape"] = [N, C, H, W]
+            result_data_dict[layer_name]["data"] = representive_samples_dict
+            result_data_dict[layer_name]["side_information"] = cluster_dict
 
-            # assert
-
-        # write input
         total_bytes = total_elements * 4  # 32-bit floating
-
-        total_gram_matrix_loss = np.sum(gram_matrix_loss)
-        print(total_gram_matrix_loss)
 
         return {
             "bytes": [
@@ -107,11 +79,42 @@ class CFP_CODEC(Bypass):
         file_prefix: str = "",
     ):
         del file_prefix
-        # print("Decode")
-        # for layer_name, layer_data in input["data"].items():
-        #     print(layer_name)
-        #     print(layer_data.shape)
-        return input
+        feature_tensor = {}
+        for layer_name, compressed_data in input.items():
+            N, C, H, W = compressed_data["original_shape"]
+            layer_data_np = np.zeros((N, C, H, W), dtype=np.float32)
+            representive_samples_dict = compressed_data["data"]
+            cluster_dict = compressed_data["side_information"]
+
+            feature_map_no_set = set()
+            total_feature_map_no_set = set(range(1, C))
+
+            for cluster_no, feature_map_no in cluster_dict.items():
+                for i in range(N):
+                    for j in feature_map_no:
+                        layer_data_np[i][j][:][:] = representive_samples_dict[
+                            cluster_no
+                        ]
+                        feature_map_no_set.add(j)
+
+            redundant_fmaps = total_feature_map_no_set - feature_map_no_set
+
+            for i in range(N):
+                for j in list(redundant_fmaps):
+                    layer_data_np[i][j][:][:] = np.zeros((H, W))
+
+            N_hat, C_hat, H_hat, W_hat = layer_data_np.shape
+
+            assert N == N_hat
+            assert C == C_hat
+            assert H == H_hat
+            assert W == W_hat
+
+            feature_tensor.update(
+                {layer_name: torch.from_numpy(layer_data_np).to("cuda")}
+            )  # get 'device'
+
+        return feature_tensor
 
     def _get_gram_matrix(self, input):
         a, b, c, d = input.size()
@@ -144,17 +147,14 @@ class CFP_CODEC(Bypass):
         self, cluster_dict, param_arr, method="mean"
     ):
         repr_cluster_dict = {}
-        for k, v in cluster_dict.items():
+        for k, v in list(cluster_dict.items()):
             # TODO: @eimran "mean" works better for now! try other methods
-            if method == "max":
-                repr_sample = np.max([param_arr[0][i][:][:] for i in v], axis=0)
-            if method == "mean":
+            if 1 <= len(v) <= 5:  # TODO: @eimran change it?!
                 repr_sample = np.mean([param_arr[0][i][:][:] for i in v], axis=0)
-            repr_cluster_dict[k] = repr_sample
-        assert len(cluster_dict) == len(
-            repr_cluster_dict
-        ), "Number of cluster didn't match with number of representative sample"
-        return repr_cluster_dict
+                repr_cluster_dict[k] = repr_sample
+            else:
+                del cluster_dict[k]
+        return cluster_dict, repr_cluster_dict
 
 
 def _number_of_elements(data: Tuple):
