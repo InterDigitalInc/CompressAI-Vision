ef5d382ace8e644fd7b6231d4916468054a0069a
Eimran Eimon <eimran.eimon@kopspgpu14p.interdigital.com>
2023-09-11 23:22:59 +0000

    tvd and hieve

---
+++ cross_checking_scripts/mpeg_cfp_hieve.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+set -eu
+
+VCM_TESTDATA=$1
+OUTPUT_DIR=$2
+EXPERIMENT=$3
+DEVICE=$4
+QP=$5
+SEQ=$6
+
+HIEVE_SRC="${VCM_TESTDATA}/HiEve_pngs"
+
+CONF_NAME="eval_cfp_codec"
+# CONF_NAME="eval_vtm"
+# CONF_NAME="eval_ffmpeg"
+
+CODEC_PARAMS=""
+# e.g.
+# CODEC_PARAMS="++codec.type=x265"
+
+CMD="compressai-vision-eval"
+
+${CMD} --config-name=${CONF_NAME}.yaml ${CODEC_PARAMS} \
+        ++pipeline.type=video \
+        ++paths._runs_root=${OUTPUT_DIR} \
+        ++pipeline.conformance.save_conformance_files=True \
+        ++pipeline.conformance.subsample_ratio=90 \
+        ++codec.encoder_config.feature_channel_suppression.manual_cluster=True \
+        ++codec.encoder_config.feature_channel_suppression.n_clusters='{75: 64, 90: 128, 105: 128}' \
+        ++codec.encoder_config.feature_channel_suppression.downscale=True \
+        ++codec.encoder_config.qp=${qp} \
+        ++codec.eval_encode='bitrate' \
+        ++codec.experiment=${EXPERIMENT} \
+        ++vision_model.arch=jde_1088x608 \
+        ++vision_model.jde_1088x608.splits="[75, 90, 105]" \
+        ++dataset.type=TrackingDataset \
+        ++dataset.settings.patch_size="[608, 1088]" \
+        ++dataset.datacatalog=MPEGHIEVE \
+        ++dataset.config.root=${HIEVE_SRC}/${SEQ} \
+        ++dataset.config.imgs_folder=img1 \
+        ++dataset.config.annotation_file=gt/gt.txt \
+        ++dataset.config.dataset_name=mpeg-hieve-${SEQ} \
+        ++evaluator.type=MOT-HIEVE-EVAL \
+        ++misc.device="${DEVICE}"
