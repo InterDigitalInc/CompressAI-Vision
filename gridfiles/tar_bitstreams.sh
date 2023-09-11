0e9c0a040bf1dcb379970cae736813567c878bd8
Eimran Eimon <eimran.eimon@kopspgpu14p.interdigital.com>
2023-09-11 22:55:01 +0000

    output saved tar filepath

--- gridfiles/tar_bitstreams.sh
+++ gridfiles/tar_bitstreams.sh
@@ -1,4 +1,10 @@
 #! /usr/bin/env bash
 output_dir=$1
 TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
-find ${output_dir} -name "*.bin" -o -name "*.dump" | tar -cf cfp_bitstream_${TIMESTAMP}.tar.gz -T -
+
+cd ${output_dir}
+
+find . -name "*.bin" -o -name "*.dump" | tar -cf cfp_bitstream_${TIMESTAMP}.tar.gz -T -
+
+echo "bitstreams tar file saved at..."
+echo ${output_dir}/cfp_bitstream_${TIMESTAMP}.tar.gz
