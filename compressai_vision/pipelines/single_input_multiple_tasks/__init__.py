f6501a13f34324aa4ddea123db6c8bfff9634ee4
Hyomin Choi <hyomin.choi@interdigital.com>
2023-10-03 17:29:40 -0700

    Quick fix: naming

--- compressai_vision/pipelines/single_input_multiple_tasks/__init__.py
+++ compressai_vision/pipelines/single_input_multiple_tasks/__init__.py
@@ -27,7 +27,7 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from .single_input_multiple_tasks import SingleInputMultipleTasks
+from .single_input_multiple_tasks_v1 import SingleInputMultipleTasks
 
 __all__ = [
     "SingleInputMultipleTasks",
