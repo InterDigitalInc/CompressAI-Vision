e9767950d300ef1f2b6b577dbd37ba97dca7273c
Fabien Racape <fabien.racape@interdigital.com>
2024-04-22 16:00:00 +0200

    Release version 1.1.8

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 package_name = "compressai_vision"
-version = "1.1.7.dev0"
+version = "1.1.8"
 git_hash = "unknown"
 
 cwd = Path(__file__).resolve().parent
