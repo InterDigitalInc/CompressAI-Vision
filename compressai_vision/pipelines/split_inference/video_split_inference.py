60782c33fb0db384bcb2127c170443e895f02208
Fabien Racape <fabien.racape@interdigital.com>
2024-08-13 14:39:15 -0700

    [fix] formatting

--- compressai_vision/pipelines/split_inference/video_split_inference.py
+++ compressai_vision/pipelines/split_inference/video_split_inference.py
@@ -290,7 +290,8 @@ class VideoSplitInference(BasePipeline):
         if self.is_mac_calculation:
             frames = (
                 len(dataloader) // 2 + 1
-                if codec.enc_tools["feature_reduction"]["temporal_resampling_enabled"] is True
+                if codec.enc_tools["feature_reduction"]["temporal_resampling_enabled"]
+                is True
                 else len(dataloader)
             )
             self.calc_kmac_per_pixels_video_task(frames, len(dataloader))
