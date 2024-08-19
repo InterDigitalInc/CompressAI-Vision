b8953e38886a7f60b8fac15cf5b90c588ee37a92
Chris Rosewarne <chris.rosewarne@canon.com.au>
2024-08-20 09:28:43 +1000

    [feat] VCMRS support for CompressAI-Vision

---
+++ scripts/datasets/sfu_to_yuv_crops.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+"""
+Extract SFU crops in YUV format from the original sequences
+"""
+
+import os
+from sfu_dicts import seq_dict, res_dict, fr_dict
+
+# Edit following paths as needed:
+SRC_YUV_DIR = os.path.expandvars('$SEQUENCE_DIR/x1100')
+DST_YUV_DIR = os.path.expandvars('$VCM_TESTDATA/SFU_HW_Obj-v3.2_supplied_jsons')
+
+
+BYTES_PER_SAMPLE = 1.5
+
+def generate_yuv_clip(sequence, sequence_name, width, height, frames_to_be_encoded, frame_skip):
+    src_yuvs = os.listdir(SRC_YUV_DIR)
+    src_yuvs = [sy for sy in src_yuvs if sy.startswith(sequence_name.replace('_val', '').replace('Kimono', 'Kimono1'))]
+    assert len(src_yuvs) == 1
+    src_yuv = os.path.join(SRC_YUV_DIR, src_yuvs[0])
+    dst_yuv = os.path.join(DST_YUV_DIR, f'{sequence_name}.yuv')
+    print(src_yuv, dst_yuv)
+
+    frame_size = int(width * height * 1.5)
+    offset = frame_size * frame_skip
+    length = frame_size * frames_to_be_encoded
+
+    with open(src_yuv, 'rb') as f:
+        f.seek(offset)
+        bytes = f.read(length)
+
+    with open(dst_yuv, 'wb') as f:
+        f.write(bytes)
+
+def main():
+    for sequence, (clas, sequence_name) in seq_dict.items():
+        width, height = res_dict[clas]
+        intra_period, frame_rate, frames_to_be_encoded, frame_skip = fr_dict[sequence]
+        generate_yuv_clip(sequence, sequence_name, width, height, frames_to_be_encoded, frame_skip)
+
+main()
--- scripts/evaluation/sfu_hw_obj/eval_on_sfu_hw_obj_vcmrs.sh
+++ scripts/evaluation/sfu_hw_obj/eval_on_sfu_hw_obj_vcmrs.sh
@@ -30,7 +30,7 @@ RUN OPTIONS:
                 [-q|--qp) quality level, depends on the inner codec, default=42]
                 [-s|--seq_name) sequence name as used in testdata root folder. E.g., "Traffic_2560x1600_30_val" in sfu_hw_obj, default="42"]
                 [-x|--extra_params) additional parameters to override default configs (pipeline/codec/evaluation...), default=""]
-EXAMPLE         [bash eval_on_mpeg_sfu_hw_vtm.sh -t /path/to/testdata -p split -i /path/to/VTM_repo -d cpu -q 32 -s Traffic_2560x1600_30_val]
+EXAMPLE         [bash eval_on_mpeg_sfu_hw_vcmrs.sh -t /path/to/testdata -p split -i /path/to/VTM_repo -d cpu -q 32 -s Traffic_2560x1600_30_val]
 _EOF_
             exit;
             ;;
@@ -42,7 +42,7 @@ _EOF_
         -d|--device) shift; DEVICE="$1"; shift; ;;
         -q|--qp) shift; QP="$1"; shift; ;;
         -s|--seq_name) shift; SEQ="$1"; shift; ;;
-        -x|--extra_params) shift; declare -a PIPELINE_PARAMS="($1)"; shift; ;;
+        -x|--extra_params) shift; PIPELINE_PARAMS="$1"; shift; ;;
         *) echo "[ERROR] Unknown parameter $1"; exit; ;;
     esac;
 done;
@@ -60,6 +60,9 @@ fi
 
 declare -A intra_period_dict
 declare -A fr_dict
+declare -A roi_descriptor
+declare -A spatial_descriptor
+
 
 intra_period_dict["Traffic_2560x1600_30_val"]=32
 fr_dict["Traffic_2560x1600_30_val"]=30
@@ -110,18 +113,18 @@ echo "============================== RUNNING COMPRESSAI-VISION EVAL== ==========
 echo "Pipeline Type:      " ${PIPELINE} " Video"
 echo "Datatset location:  " ${FCM_TESTDATA}
 echo "Output directory:   " ${OUTPUT_DIR}
-echo "Experiment folder:  " "vtm"${EXPERIMENT}
+echo "Experiment folder:  " "vcmrs"${EXPERIMENT}
 echo "Running Device:     " ${DEVICE}
 echo "Input sequence:     " ${SEQ}
 echo "Seq. Framerate:     " ${FRAME_RATE}
 echo "QP for Inner Codec: " ${QP}
 echo "Intra Period for Inner Codec: "${INTRA_PERIOD}
-echo "Other Parameters:   " ${PIPELINE_PARAMS[@]}
+echo "Other Parameters:   " ${PIPELINE_PARAMS}
 echo "=================================================================================================="
 
 compressai-${PIPELINE}-inference --config-name=${CONF_NAME} \
         ++pipeline.type=video \
-        ++pipeline.codec.vcm_mode=True \
+	++pipeline.codec.vcm_mode=True \
         ++paths._run_root=${OUTPUT_DIR} \
 	++vision_model.arch=faster_rcnn_X_101_32x8d_FPN_3x \
         ++dataset.type=Detectron2Dataset \
@@ -131,17 +134,13 @@ compressai-${PIPELINE}-inference --config-name=${CONF_NAME} \
         ++dataset.config.dataset_name=sfu-hw-${SEQ} \
         ++evaluator.type=COCO-EVAL \
         ++codec.experiment=${EXPERIMENT} \
-	codec=vtm.yaml \
+	codec=vcmrs.yaml \
         ++codec.encoder_config.intra_period=${INTRA_PERIOD} \
         ++codec.encoder_config.parallel_encoding=True \
         ++codec.encoder_config.qp=${QP} \
-        ++codec.codec_paths.encoder_exe=${INNER_CODEC_PATH}'/bin/EncoderAppStatic'  \
-        ++codec.codec_paths.decoder_exe=${INNER_CODEC_PATH}'/bin/DecoderAppStatic' \
-        ++codec.codec_paths.parcat_exe=${INNER_CODEC_PATH}'/bin/parcatStatic' \
-        ++codec.codec_paths.cfg_file=${INNER_CODEC_PATH}'/cfg/encoder_lowdelay_vtm.cfg' \
+        ++codec.encoder_config.input_bitdepth=8 \
         ++codec.eval_encode='bitrate' \
         ++codec.verbosity=0 \
 	++codec.device=${DEVICE} \
         ++misc.device.nn_parts=${DEVICE} \
-        "${PIPELINE_PARAMS[@]}" \
-
+        ${PIPELINE_PARAMS} \
