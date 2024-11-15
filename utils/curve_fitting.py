bb15e849015e9232abf4746d8c464ed7fe1204ef
Chris Rosewarne <chris.rosewarne@canon.com.au>
2024-11-15 14:01:01 +1100

    [feat] --nb_operation_points = 0 allows any number of results. Modify script name that now generates csv results in both FCM and VCM formats

--- utils/curve_fitting.py
+++ utils/curve_fitting.py
@@ -1,6 +1,6 @@
-# The copyright in this software is being made available under the BSD 
+# The copyright in this software is being made available under the BSD
 # License, included below. This software may be subject to other third party
-# and contributor rights, including patent rights, and no such rights are 
+# and contributor rights, including patent rights, and no such rights are
 # granted under this license.
 
 # Copyright (c) 2022, ISO/IEC
@@ -14,12 +14,12 @@
 #  * Redistributions in binary form must reproduce the above copyright notice,
 #    this list of conditions and the following disclaimer in the documentation
 #    and/or other materials provided with the distribution.
-#  * Neither the name of the ISO/IEC nor the names of its contributors may 
+#  * Neither the name of the ISO/IEC nor the names of its contributors may
 #    be used to endorse or promote products derived from this software without
 #    specific prior written permission.
 
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 # ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS
 # BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
