287343a0b8a6ac15b4ccb5c6f472a9b1be3e8fdf
Heeji Han <sarhakor@naver.com>
2025-01-14 15:59:06 +0000

    [fix] modify dictionary key values ​​for class-wise calculations

--- utils/compute_overall_kmac_per_px.py
+++ utils/compute_overall_kmac_per_px.py
@@ -38,12 +38,9 @@ import pandas as pd
 
 
 def generate_csv_classwise_image_gmac(dataset_name, result_path, list_of_classwise_seq):
-    prefix = "mpeg-oiv6"
     seq_wise_results = []
-    cls_wise_results = []
     for cls_seqs in list_of_classwise_seq:
-        complexity_lst_class_wise = []
-        seq_name = f"{prefix}-{cls_seqs}"
+        seq_name = f"mpeg-oiv6-{cls_seqs}"
         base_path = f"{result_path}/{seq_name}"
         qps = [
             f
@@ -55,11 +52,15 @@ def generate_csv_classwise_image_gmac(dataset_name, result_path, list_of_classwi
             complexity_dict = {"Dataset": seq_name, "pp": idx}
             comp_path = f"{base_path}/{qp}/evaluation/summary_complexity.csv"
             summary_path = f"{base_path}/{qp}/evaluation/summary.csv"
+            
+            if not (os.path.exists(summary_path) or os.path.exists(summary_path)):
+                continue
+            
             with open(summary_path, mode="r", newline="", encoding="utf-8") as file:
                 reader = csv.DictReader(file)
                 data = [row for row in reader][0]
                 complexity_dict["qp"] = data["qp"]
-
+            
             with open(comp_path, mode="r", newline="", encoding="utf-8") as file:
                 reader = csv.DictReader(file)
                 data = [row for row in reader][0]
@@ -69,55 +70,34 @@ def generate_csv_classwise_image_gmac(dataset_name, result_path, list_of_classwi
             for k, v in kmac_per_pixels.items():
                 complexity_dict[k] = v
 
-            complexity_lst_class_wise.append(complexity_dict)
             seq_wise_results.append(complexity_dict)
 
-        # class-wise calculation
-        nn_part1_lst, ft_reduction_lst, ft_restoration_lst, nn_part2_lst = (
-            [],
-            [],
-            [],
-            [],
-        )
-        for seq_data in complexity_lst_class_wise:
-            nn_part1_lst.append(seq_data["nn_part1"])
-            ft_reduction_lst.append(seq_data["feature reduction"])
-            ft_restoration_lst.append(seq_data["feature restoration"])
-            nn_part2_lst.append(seq_data["nn_part2"])
-
-        cls_wise_result = {
-            "Dataset": f"{seq_name}-avg",
-            "pp": 0,
-            "qp": 0,
-            "nn_part1": sum(nn_part1_lst) / len(nn_part1_lst),
-            "feature reduction": sum(ft_reduction_lst) / len(ft_reduction_lst),
-            "feature restoration": sum(ft_restoration_lst) / len(ft_restoration_lst),
-            "nn_part2": sum(nn_part2_lst) / len(nn_part2_lst),
-        }
-
-        cls_wise_results.append(cls_wise_result)
-
-    results = pd.DataFrame(seq_wise_results + cls_wise_results)
-
-    return results
+    return pd.DataFrame(seq_wise_results)
 
 
 def generate_csv_classwise_video_gmac(
     dataset_name, result_path, list_of_classwise_seq, seq_lst
 ):
-    if dataset_name == "SFU":
-        prefix = "sfu-hw"
-    else:
-        prefix = "mpeg"
 
-    seq_wise_results = []
-    cls_wise_results = []
+    seq_base_path = [
+                    f
+                    for f in os.listdir(result_path)
+                    if os.path.isdir(os.path.join(result_path, f))
+               ]
+
+    seq_wise_results, cls_wise_results = [], []
     for cls_seqs in list_of_classwise_seq:
         for cls_name, seqs in cls_seqs.items():
             complexity_lst_class_wise = []
-            for seq in seqs:
-                seq_name = [seq_name for seq_name in seq_lst if seq in seq_name][0]
-                base_path = f"{result_path}/{prefix}-{seq_name}_val"
+            
+            seq_path = [
+                next(name for name in seq_base_path if s in name) 
+                for s in seqs
+                if any(s in name for name in seq_base_path)
+            ]
+            
+            for seq in seq_path:
+                base_path = f"{result_path}/{seq}"
                 qps = [
                     f
                     for f in os.listdir(base_path)
@@ -125,7 +105,7 @@ def generate_csv_classwise_video_gmac(
                 ]
                 qps = sorted(qps)
                 for idx, qp in enumerate(qps):
-                    complexity_dict = {"Dataset": seq_name, "pp": idx}
+                    complexity_dict = {"Dataset": seq, "pp": idx}
                     comp_path = f"{base_path}/{qp}/evaluation/summary_complexity.csv"
                     summary_path = f"{base_path}/{qp}/evaluation/summary.csv"
                     with open(
@@ -152,7 +132,7 @@ def generate_csv_classwise_video_gmac(
                     seq_wise_results.append(complexity_dict)
 
             # class-wise calculation
-            for idx in range(4):
+            for idx in range(len(qps)):
                 (
                     nn_part1_lst,
                     ft_reduction_lst,
@@ -298,7 +278,7 @@ if __name__ == "__main__":
         )
     elif args.dataset_name == "HIEVE":
         hieve_1080p = {"HIEVE-1080P": ["13", "16"]}
-        hieve_720p = {"HIEVE-720P": ["2", "17", "18"]}
+        hieve_720p = {"HIEVE-720P": ["17", "18", "2"]}
         seq_list = [
             "13_1920x1080_30",
             "16_1920x1080_30",
@@ -309,12 +289,7 @@ if __name__ == "__main__":
         output_df = generate_csv_classwise_video_gmac(
             args.dataset_name, args.result_path, [hieve_1080p, hieve_720p], seq_list
         )
-        # sort for FCM template - comply with the template provided in wg04n00459
 
-        sorterIndex = dict(zip(seq_list, range(len(seq_list))))
-        output_df["ds_rank"] = output_df["Dataset"].map(sorterIndex)
-        output_df.sort_values(["ds_rank", "qp"], ascending=[True, True], inplace=True)
-        output_df.drop(columns=["ds_rank"], inplace=True)
     else:
         raise NotImplementedError
 
